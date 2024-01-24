7f5848c61351a876ba7903f0093a4745d9c2e1ee
kiko73 <kikovalledelrio@gmail.com>
2024-01-24 13:48:09 +0100

    funcion mover nave y asteroides

--- figura_class.py
+++ figura_class.py
@@ -1,16 +1,28 @@
 import pygame as pg
 
 class Nave:
-    def __init__(self, pos_x,pos_y,color=(255,255,255),w=60,h=20):
+    def __init__(self, pos_x,pos_y,color=(255,255,255),w=60,h=60,vx=1,vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
         self.w = w
         self.h = h
+        self.vx = vx
+        self.vy = vy
         
 
     def dibujar(self,surface):
         pg.draw.rect(surface,self.color,(self.pos_x,self.pos_y,self.w,self.h))
+
+    def mover(self,teclado_arriba,teclado_abajo):
+        estado_teclado = pg.key.get_pressed()
+
+        if estado_teclado[teclado_arriba] == True and self.pos_y >= 0+(self.h//2):
+            self.pos_y -= 1
+        
+        if estado_teclado[teclado_abajo] == True and self.pos_y <= 700-(self.h//2):
+            self.pos_y += 1
+        
         
 
 class Asteroide:
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame as pg
 from figura_class import Asteroide,Nave
+import random as ra
 
 pg.init()
 
 pantalla_principal = pg.display.set_mode((1300,700))
 pg.display.set_caption("The Quest")
 
+tasa_refresco = pg.time.Clock()
+"""
 asteroide = Asteroide(1250,350,(218, 99, 35),50)
 asteroide1 = Asteroide(600,600,(195, 238, 19),60)
 asteroide2 = Asteroide(800,200,(135, 19, 238),40)
@@ -16,20 +19,33 @@ asteroide6 = Asteroide(650,300,(238, 42, 19),70)
 asteroide7 = Asteroide(400,550,(49, 19, 238),20)
 asteroide8 = Asteroide(200,600,(238, 19, 49),20)
 asteroide9 = Asteroide(800,650,(221, 19, 238),50)
-
-nave = Nave(0, 350 - 30,(255,255,255),60,60)
+"""
+nave = Nave(10, 350 - (60//2))
 
 game_over = True
 
+lista_asteroides=[]
+for i in range(1,10):
+   lista_asteroides.append(Asteroide(ra.randit(0,1200),ra.randint(0,600),(ra.randit(0,255),ra.randit(0,255),ra.randit(0,255)),radio=ra.randint=(10.100)))
+   
+
+
+
 while game_over:
+
+   valor_tasa = tasa_refresco.tick(350)
+  
    
 
    for evento in pg.event.get():
       if evento.type == pg.QUIT:
          game_over = False
 
+
    pantalla_principal.fill((25, 27, 18 ))
    
+   nave.mover(pg.K_UP,pg.K_DOWN)
+   """
    asteroide.dibujar(pantalla_principal)
    asteroide1.dibujar(pantalla_principal)
    asteroide2.dibujar(pantalla_principal)
@@ -40,7 +56,7 @@ while game_over:
    asteroide7.dibujar(pantalla_principal)
    asteroide8.dibujar(pantalla_principal)
    asteroide9.dibujar(pantalla_principal)
-
+   """
    nave.dibujar(pantalla_principal)
    
    pg.display.flip()
